e2d595dd2876127c22ffcdb14cac557996626aaa
tun43p <git@tun43p.com>
2022-03-11 23:07:38 +0100

    feat: connect wallet and contract and update configuration

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import ReactDOM from "react-dom";
+import Provider, { ProviderContext } from "./utils/provider";
 
 function App(): JSX.Element {
+  const { account, connect, contract } = useContext(ProviderContext);
+
   return (
     <div>
       <h1>Dapp Template</h1>
+      <button onClick={connect}>{account ? account : "Connect wallet"}</button>
+      {account && contract && <p>Account and contract connected</p>}
     </div>
   );
 }
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById("app")
+  document.getElementById("app"),
 );
--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="react-scripts" />
+
+import { ethers } from "ethers";
+
+declare global {
+  interface Window {
+    ethereum: Ethereum;
+  }
+
+  type Ethereum =
+    | {
+        request: (params: { method: string }) => Promise<string[]>;
+      }
+    | undefined;
+
+  type Contract = ethers.Contract;
+  type Signer = ethers.Signer;
+  type Web3Provider = ethers.providers.Web3Provider;
+}
